8ba3509b2e4f39458e987df9eb66e270af4b86f8
Kevin <fraesh93@gmail.com>
2024-01-27 16:11:09 +0100

    convert full db

--- scripts/convertDB.js
+++ scripts/convertDB.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const convertDb = async () => {
   const res = await fetch("https://fraesh.github.io/vps-db/vpsdb.json");
   let data = await res.json();
-  data = data.slice(0, 10);
+
   for (let game of data) {
     let gameData = JSON.stringify(game);
-    gameData.replace(
+    gameData = gameData.replaceAll(
       "https://fraesh.github.io",
       "https://virtualpinballspreadsheet.github.io"
     );
